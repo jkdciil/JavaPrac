4e9198442e18e5c4d7bbcdd05e9eaeb53e575b14
jinka vishnu vardhan <pandumpd8142@gmail.com>
2022-06-08 12:44:42 +0530

    this is my second commit

---
+++ src/DailyPractice/GettersAndSettersChild.java
@@ -0,0 +1,12 @@
+package DailyPractice;
+
+public class GettersAndSettersChild {
+
+	public static void main(String[] args) {
+		GettersAndSettersParent gp=new GettersAndSettersParent();
+		gp.setters("name");
+		System.out.println(gp.getters());
+
+	}
+
+}
---
+++ src/DailyPractice/GettersAndSettersParent.java
@@ -0,0 +1,16 @@
+package DailyPractice;
+
+public class GettersAndSettersParent {
+
+	private String name;
+	
+	public String getters()
+	{
+		return name;
+	}
+	public void setters(String name)
+	{
+		this.name=name;
+		System.out.println("some one is using your name");
+	}
+}
