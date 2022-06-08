7656db3157f472580833d3fadadd33b6bfea77e8
jinka vishnu vardhan <pandumpd8142@gmail.com>
2022-06-08 13:24:00 +0530

    normal

--- src/DailyPractice/loops.java
+++ src/DailyPractice/loops.java
@@ -5,8 +5,8 @@ public class loops {
 	public static void main(String[] args) {
 		for(int i=0;i<=5;i++)
 		{
-			System.out.println(i);
-			i++;
+			System.out.println(i);//this will print 0-5
+			i++;//to print even values 
 		}
 
 	}
---
+++ src/DailyPractice/normal.java
@@ -0,0 +1,10 @@
+package DailyPractice;
+
+public class normal {
+
+	public static void main(String[] args) {
+		System.out.println("hi how are you");
+
+	}
+
+}
