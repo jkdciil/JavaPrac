595d04d3c3af4e6d2693a08567b1d47787255d79
jinka vishnu vardhan <pandumpd8142@gmail.com>
2022-06-15 11:42:01 +0530

    websites for practice

---
+++ src/DailyPractice/PracticeSites.java
@@ -0,0 +1,22 @@
+package DailyPractice;
+
+public class PracticeSites {
+
+	public static void main(String[] args) {
+		
+		//http://itera-qa.azurewebsite.net/home/automation
+		//http://the-internet.herokuapp.com/
+		//http://www.globalsga.com/demo-site/
+		//http://testautomationpractice.blogspot.com/
+		//http://www.saucedemo.com/
+		
+		       //INTERMEDIATE&ADVANCED
+		
+		//http://demo.nopcommerce.com/
+		//http://opensource-demo.orangehrmlive.com/
+		//http://admin-demo.nopcommerce.com/
+		//http://demo.opencart.com/
+
+	}
+
+}
